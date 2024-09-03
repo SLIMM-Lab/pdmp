fixed computation of hessian of model and likelihood

src/distributions.py
@@ -42,12 +42,18 @@ class MultivariateNormal:
 
     def logDensity(self, x):
         diff = x - self.mean_
-        return self.constant_ - self.logDet_ - 0.5 * np.linalg.norm(np.linalg.solve(self.covL_, diff)) ** 2
+        if diff.ndim == 1:
+            if self.dim_ == 1:
+                return self.constant_ - self.logDet_ - 0.5 * np.abs(diff / self.covL_[0,0]) ** 2
+            else:
+                return self.constant_ - self.logDet_ - 0.5 * np.linalg.norm(np.linalg.solve(self.covL_, diff)) ** 2
+        else:
+            return (self.constant_ - self.logDet_ - 0.5 * np.linalg.norm(np.linalg.solve(self.covL_, diff.T), axis=0) ** 2).T
 
     # remove unnecessary argument if zagzag directly samples on gaussian
     # def gradLogDensity(self, x, sub_sampling=False):
     def gradLogDensity(self, x):
-        diff = x - self.mean_
+        diff =  x - self.mean_
         return - sp.linalg.solve_triangular(self.covL_.transpose(),
                                             sp.linalg.solve_triangular(self.covL_, diff, lower=True))
 
@@ -130,6 +136,7 @@ class MultivariateLogNormal:
 class Likelihood:
     def __init__(self, model, x_obs, u_obs, sigma_obs):
         self.model_ = model
+        self.n_params_ = model.getDim()
         self.x_obs_ = x_obs
         self.u_obs_ = u_obs
         self.n_obs_ = self.u_obs_.shape[0]
@@ -159,7 +166,7 @@ class Likelihood:
     def gradLogDensity(self, params, idx=None):
 
         if idx is None:
-            grad = np.zeros(self.dim_)
+            grad = np.zeros(self.n_params_)
             for i in range(self.n_obs_):
                 m = self.model_.eval(self.x_obs_, params, idx=i)
                 grad_m = self.model_.eval_grad(self.x_obs_, params, idx=i)
@@ -171,17 +178,17 @@ class Likelihood:
             grad_m = self.model_.eval_grad(self.x_obs_, params, idx=idx)
             return self.dists_[idx].gradLogDensity(m) @ grad_m
 
-    # todo: fix this implementation
-    def hessianLogDensity(self, params):
+    def hessianLogDensity(self, params: np.ndarray) -> np.ndarray:
 
-        m = self.model_.eval(self.x_obs_, params)
-        grad_m = self.model_.eval_grad(self.x_obs_, params)
-        hess_m = self.model_.eval_hessian(self.x_obs_, params)
-        hess = np.zeros((self.dim_, self.dim_))
+        hess = np.zeros((self.n_params_, self.n_params_))
 
         for i in range(self.n_obs_):
-            hess += self.dists_[i].gradLogDensity(m) @ hess_m + \
-                    self.dists_[i].hessianLogDensity(m) @ grad_m @ grad_m.T
+            m = self.model_.eval(self.x_obs_, params, idx=i)
+            grad_m = self.model_.eval_grad(self.x_obs_, params, idx=i)
+            hess_m = self.model_.eval_hessian(self.x_obs_, params, idx=i)
+            hess += np.einsum('ij,jk,il', self.dists_[i].hessianLogDensity(m), grad_m, grad_m)
+            hess += np.einsum('i,ijk->jk', self.dists_[i].gradLogDensity(m), hess_m)
+
         return hess
 
 class FlatLikelihood:

src/forward_model.py
@@ -5,7 +5,6 @@ import sympy as sy
 
 class ForwardModel:
     def __init__(self, F, n_params):
-        # self.F = F
         self.F = sy.symbols('F')
         self.F_vals = np.array(F)
         self.n_settings_ = self.F_vals.shape[0]
@@ -14,7 +13,6 @@ class ForwardModel:
         self.params_val = np.zeros(n_params)
         self.x = sy.symbols('x')
         self.u = sy.symbols('u', cls=sy.Function)
-        # C1 = sy.symbols('C1')
         offset = 1.
 
         self.E = offset + (self.params[0] - offset) * sy.Heaviside(self.x)
@@ -48,11 +46,6 @@ class ForwardModel:
         print("\n")
         self.hessian = [[sy.lambdify((self.x, self.F, *self.params), hess, 'numpy') for hess in hessian_row] for hessian_row in hessian]
 
-        # print(self.gradient)
-
-    # def set_params_vals(self, params_val):
-    #     self.params_val = params_val
-
     def eval_E(self, x, params):
         E = self.E.subs([*zip(self.params, params)])
         return np.array([E.subs(self.x, x_i) for x_i in x])
@@ -60,18 +53,30 @@ class ForwardModel:
     def eval(self, x, params, idx=None):
         if idx is None:
             idx = 0
-        return self.w_np(x, self.F_vals[idx], *params)
+
+        if len(params) == self.n_params:
+            return self.w_np(x, self.F_vals[idx], *params)
+        else:
+            if len(params.shape) == 1:
+                params=params[:, None]
+            assert params.shape[1] == self.n_params, "Array dimensions do not match"
+            return self.w_np(x, self.F_vals[idx], *[param[:, None] for param in params.T])
 
     def eval_grad(self, x, params, idx=0):
         if idx is None:
             idx = 0
         return np.array([[grad(x_i, self.F_vals[idx], *params) for grad in self.gradient] for x_i in x])
 
-    # todo: fix implementation
     def eval_hessian(self, x, params, idx=0):
         if idx is None:
             idx = 0
-        return np.array([[[hess(x_i, self.F_vals[idx], *params) for hess in hessian_row] for x_i in x] for hessian_row in self.hessian])
+
+        hessian = np.zeros((len(x), self.n_params, self.n_params))
+        for i, x_i in enumerate(x):
+            for j, hessian_row in enumerate(self.hessian):
+                for k, hess in enumerate(hessian_row):
+                    hessian[i, j, k] = hess(x_i, self.F_vals[idx], *params)
+        return hessian
 
     # todo: remove camel case notation
     def getDim(self):