7861adc882a437c2b5751e068e58d8fac0628ef3
leon@workstation_home_new <l.f.riccius@tudelft.nl>
2024-09-16 13:05:25 +0200

    added scripts to create em_poster figures

---
+++ em_poster/laplace_demo.py
@@ -0,0 +1,150 @@
+import os.path
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.optimize import minimize
+
+from src.forward_model import ForwardModel
+from src.project_field import compute_coefficients, squared_exponential_kernel, PiecewiseConstantBasis
+from src.distributions import MultivariateNormal, GaussianLikelihood, Posterior
+from src.samplers import ZigZagSampler, MetropolisHastingsSampler
+from src.utils import plot_pdf_contours, plot_samples, get_2d_despined_figure
+
+
+sns.set_style('white')
+rng = np.random.default_rng(0)
+
+save_fig = False
+fig_path = '/home/leon/ownCloud/Documents/presentations/em/symposium_25/figures'
+
+if __name__ == '__main__':
+
+    # check if the directory exists, otherwise create
+    if save_fig and not os.path.exists(fig_path):
+        os.makedirs(fig_path)
+
+    # get the prior field
+    kernel_params = {'sigma': 1., 'l': 0.3}
+    n_b = 2
+    interval = (0, 1)
+    basis = PiecewiseConstantBasis(n_b, interval)
+    prior_cov = compute_coefficients(squared_exponential_kernel, basis, interval, kernel_params=kernel_params)
+    mean = 4.
+    prior = MultivariateNormal(mean * np.ones(n_b), prior_cov, rng=rng)
+
+    # set up the forward model
+    n_obs = 2
+    F = [1.]
+    F = [item for item in F for i in range(n_obs)]
+    n_params = n_b
+    model = ForwardModel(F, n_params)
+
+    # generate ground truth from a multi-variate normal distribution
+    mean = 3 * np.ones(n_params)
+    cov = np.eye(n_params)
+    params_gt = rng.multivariate_normal(mean, cov)
+    print(f"Ground truth: \n {params_gt}")
+    margins = 1.5
+    plot_limits = ([2.3, 4.1], [2.05, 4.15])
+
+    # define observation and test locations
+    x_obs = np.linspace(0, 1, 3)[1:]
+    x_test = np.linspace(0, 1, 100)
+
+    # generate n_obs noisy obersevations of u_gt
+    sigma_obs = 0.025
+
+    # evaluate the ground truth at the observation and test locations
+    u_gt = model.eval(x_obs, params_gt)
+    u_gt_test = model.eval(x_test, params_gt)
+    u_obs = np.zeros((len(F), u_gt.shape[0]))
+
+    for i in range(len(F)):
+        u_gt = model.eval(x_obs, params_gt, idx=i)
+        u_obs[i] = u_gt + rng.normal(0, sigma_obs, (1, u_gt.shape[0]))
+
+    # define likelihood and posterior
+    likelihood = GaussianLikelihood(model, x_obs, u_obs, sigma_obs)
+    target = Posterior(prior, likelihood)
+
+    # ---------------------------------- laplace approx ----------------------------------------------
+    # define negative log posterior and gradient for optimization
+    log_post = lambda x: target.log_density(x)
+    n_log_post = lambda x: - log_post(x)
+    n_grad_log_post = lambda x: - target.grad_log_density(x)
+
+    # find the minimum of the function with bfgs
+    x0 = np.array([4., 4.])
+    opt = minimize(n_log_post, x0, method='BFGS', jac=n_grad_log_post)
+    x_map = opt.x
+    print(opt)
+
+    hess_an = target.hessian_log_density(x_map)
+    print(f"Hess an: \n{hess_an}")
+
+    cov_map = np.linalg.inv(- hess_an)
+    laplace_approx = MultivariateNormal(x_map, cov_map, rng=rng)
+    # get figure and plot pdfs
+    scale = 1.2
+    figsize = scale * np.abs(plot_limits[0][1] - plot_limits[0][0]), scale * np.abs(plot_limits[1][1] - plot_limits[1][0])
+    # fig, ax = get_2d_despined_figure(plot_limits, figsize=(scaling * 3, scaling *3.2))
+    fig, ax = get_2d_despined_figure(plot_limits, figsize=figsize, axis_label='E')
+    ax.plot(x_map[0], x_map[1], 'ro')
+    plot_pdf_contours(target, ax, plot_limits, alpha=0.3)
+    plot_pdf_contours(laplace_approx, ax, plot_limits, n_levels=10, alpha=1.0, cmap=sns.color_palette('mako', as_cmap=True))
+    ax.set_xlim(plot_limits[0])
+    ax.set_ylim(plot_limits[1])
+
+    if save_fig:
+        fig.savefig(os.path.join(fig_path, 'laplace_approx_contours.pdf'))
+
+    plt.show()
+
+    # ---------------------------------- zig zag sampler ----------------------------------------------
+    fig, ax = get_2d_despined_figure(plot_limits, figsize=figsize, keep_ticks=False, axis_label='E')
+    plot_pdf_contours(target, ax, plot_limits)
+
+    n_events = 2000
+    n_accepted = 200
+    approx = {'mean': x_map, 'inv_cov': - hess_an}
+    x0 = np.array([3.5, 2.5])
+    sampler = ZigZagSampler(target, n_events=n_events, rng=rng, approximation=approx, x0=x0,
+                            n_events_accepted=n_accepted)
+    sampler.run()
+
+    positions = sampler.positions_
+    ax.plot(positions[:, 0], positions[:, 1], c='C0', alpha=0.75, linewidth=1.)
+
+    if save_fig:
+        fig.savefig(os.path.join(fig_path, f'zig_zag_thinning_{n_accepted}.pdf'))
+
+    plt.show()
+
+    # ------------------------- RWMH -------------------------
+    # get 'true' mean and variance from mcmc
+    n_samples = 2000
+    mh_sampler = MetropolisHastingsSampler(target, n_samples=n_samples, sigma=np.sqrt(.5), rng=rng,
+                                           prec=cov, cov_factor=0.5)
+    mh_sampler.run()
+    mh_samples = mh_sampler.chain_
+
+    # get figure and plot pdfs
+    fig, ax = get_2d_despined_figure(plot_limits, figsize=figsize, axis_label='E')
+    plot_pdf_contours(target, ax, plot_limits)
+    plot_samples(mh_samples, ax, color_code=False, n_vis=2000, size=1.5)
+
+    if save_fig:
+        fig.savefig(os.path.join(fig_path, f'random_walk_{n_samples}.pdf'))
+
+    plt.show()
+
+    # MH with 200 samples
+    n_samples = 200
+    fig, ax = get_2d_despined_figure(plot_limits, figsize=figsize, axis_label='E')
+    plot_pdf_contours(target, ax, plot_limits)
+    plot_samples(mh_samples[:n_samples], ax, color_code=False, n_vis=500, size=1.5)
+
+    if save_fig:
+        fig.savefig(os.path.join(fig_path, f'random_walk_{n_samples}.pdf'))
+
+    plt.show()
