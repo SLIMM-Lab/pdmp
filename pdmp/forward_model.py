f4d5323d9ef04355cb67dfc27979a51ac9d95ec7
leon@workstation_home_new <l.f.riccius@tudelft.nl>
2024-12-10 18:00:20 +0100

    added baseclass for forward model and provided propper code documentation

--- pdmp/forward_model.py
+++ pdmp/forward_model.py
@@ -3,96 +3,225 @@ import matplotlib.pyplot as plt
 import sympy as sy
 
 
-class ForwardModel:
-    def __init__(self, F, n_params):
-        self.F = sy.symbols('F')
-        self.F_vals = np.array(F)
-        self.n_settings_ = self.F_vals.shape[0]
-        self.n_params = n_params
-        self.params = sy.symbols([f'p_{str(i)}' for i in range(n_params)])
-        self.params_val = np.zeros(n_params)
-        self.x = sy.symbols('x')
-        self.u = sy.symbols('u', cls=sy.Function)
-        offset = 1.
+class Model:
+    """
+    Base class for the forward model
+    """
+    def __init__(self):
+        pass
 
+    def eval(self, params: np.ndarray, **kwargs) -> np.ndarray:
+        """
+        Evaluate the forward model
+        Args:
+            params (np.ndarray): parameter values
+            **kwargs: additional arguments
+        Returns:
+            np.ndarray: model evaluation
+        """
+        raise NotImplementedError
+
+    def eval_grad(self, params: np.ndarray, **kwargs) -> np.ndarray:
+        """
+        Evaluate the gradient (jacobian) of the forward model outputs with respect to the parameters
+        Args:
+            params (np.ndarray): parameter values
+            **kwargs: additional arguments
+        Returns:
+            np.ndarray: gradient of model evaluation
+        """
+        raise NotImplementedError
+
+    def eval_hessian(self, params: np.ndarray, **kwargs) -> np.ndarray:
+        """
+        Evaluate the hessian of the forward model outputs with respect to the parameters
+        Args:
+            params (np.ndarray): parameter values
+            **kwargs: additional arguments
+        Returns:
+            np.ndarray: hessian of model evaluation
+        """
+        raise NotImplementedError
+
+    def get_dim(self) -> int:
+        """
+        Get dimension of the model outputs
+        Returns:
+            int: dimension of the model
+        """
+        raise NotImplementedError
+
+    def get_n_settings(self) -> int:
+        """
+        Get number of settings.
+
+        Returns:
+            int: Number of settings. Default is 1 if not overridden by derived classes.
+        """
+        return 1
+
+class PiecewiseConstantModel(Model):
+    """
+    Forward model for deformation of a 1d bar with piecewise constant Young's modulus
+    """
+    def __init__(self, F: np.ndarray, n_params: int, x_obs: np.ndarray):
+        """
+        Initialize the model
+        Args:
+            F (np.ndarray): collection of prescribed forces for each setting
+            n_params (int): number of parameters
+            x_obs (np.ndarray): observed x values
+        """
+        super().__init__()
+        self.F = sy.symbols('F')  # Symbolic representation of F
+        self.F_vals = F  # Actual values of F
+        self.n_settings_ = self.F_vals.shape[0]  # Number of settings
+        assert self.n_settings_ == len(x_obs), "Number of settings does not match number of observations"
+        self.x_obs_ = x_obs  # Observations
+
+        self.n_params = n_params  # Number of parameters
+        self.params = sy.symbols([f'p_{str(i)}' for i in range(n_params)])  # Symbolic parameters
+        self.params_val = np.zeros(n_params)  # Initial parameter values
+        self.x = sy.symbols('x')  # Symbolic variable x
+        self.u = sy.symbols('u', cls=sy.Function)  # Symbolic function u
+        offset = 1.  # Offset for Young's modulus
+
+        # Define Young's modulus as a piecewise function
         self.E = offset + (self.params[0] - offset) * sy.Heaviside(self.x)
         for i in range(n_params - 1):
             self.E += (self.params[i + 1] - self.params[i]) * sy.Heaviside(self.x - (i + 1) / self.n_params)
         self.E = self.E.rewrite(sy.Piecewise)
         print(f"Young's modulus:\n{self.E}\n")
 
-        u_i = []
-        conditions = []
+        u_i = []  # List to store piecewise functions for u
+        conditions = []  # List to store conditions for piecewise functions
 
+        # Define piecewise functions for u
         for i in range(self.n_params):
             u_i.append(sy.S((self.x - (i/self.n_params)) / self.params[i]))
             conditions.append(sy.S(self.x < (i + 1) / self.n_params))
             for j in range(i):
                 u_i[i] += (1 / self.n_params) / self.params[j]
 
-        conditions[-1] = sy.S('1')
-        self.u = self.F * sy.Piecewise(*zip(u_i, conditions))
-        self.u_np = sy.lambdify((self.x, self.F, *self.params), self.u, 'numpy')
+        conditions[-1] = sy.S('1')  # Last condition is always true
+        self.u = self.F * sy.Piecewise(*zip(u_i, conditions))  # Define u as a piecewise function
+        self.u_np = sy.lambdify((self.x, self.F, *self.params), self.u, 'numpy')  # Convert u to a numpy function
 
+        # Compute gradient of u with respect to parameters
         gradient = [sy.diff(self.u, param) for param in self.params]
         print("Gradient:\n")
         [print(grad) for grad in gradient]
         print("\n")
         self.gradient = [sy.lambdify((self.x, self.F, *self.params), grad, 'numpy') for grad in gradient]
 
+        # Compute Hessian of u with respect to parameters
         hessian = [[sy.diff(grad, param) for param in self.params] for grad in gradient]
         print("Hessian:\n")
         [[print(hess) for hess in hessian_row] for hessian_row in hessian]
         print("\n")
         self.hessian = [[sy.lambdify((self.x, self.F, *self.params), hess, 'numpy') for hess in hessian_row] for hessian_row in hessian]
 
-    def eval_E(self, x, params):
-        E = self.E.subs([*zip(self.params, params)])
-        return np.array([E.subs(self.x, x_i) for x_i in x])
+    def eval_E(self, params: np.ndarray,x: np.ndarray) -> np.ndarray:
+        """
+        Evaluate Young's modulus at given x values
+        Args:
+            params (np.ndarray): parameter values
+            x (np.ndarray): x values
+        Returns:
+            np.ndarray: Young's modulus evaluated at x
+        """
+        E = self.E.subs([*zip(self.params, params)])  # Substitute parameter values into E
+        return np.array([E.subs(self.x, x_i) for x_i in x])  # Evaluate E at each x value
 
-    def eval(self, x, params, idx=None):
+    def eval(self, params: np.ndarray, x: np.ndarray=None, idx: int=None) -> np.ndarray:
+        """
+        Evaluate displacements u at given x locations for setting idx
+        Args:
+            params (np.ndarray): parameter values
+            x (np.ndarray): x values
+            idx (int): setting index
+        Returns:
+            np.ndarray: u evaluated at x
+        """
+        if x is None:
+            x = self.x_obs_  # Use observed x values if none provided
+
         if idx is None:
-            idx = 0
+            idx = 0  # Default index is 0
 
         if len(params) == self.n_params:
-            return self.u_np(x, self.F_vals[idx], *params)
+            return self.u_np(x, self.F_vals[idx], *params)  # Evaluate u with given parameters
         else:
             if len(params.shape) == 1:
-                params=params[:, None]
+                params = params[:, None]  # Reshape parameters if necessary
             assert params.shape[1] == self.n_params, "Array dimensions do not match"
             return self.u_np(x, self.F_vals[idx], *[param[:, None] for param in params.T])
 
-    def eval_grad(self, x, params, idx=0):
-        if idx is None:
-            idx = 0
-        return np.array([[grad(x_i, self.F_vals[idx], *params) for grad in self.gradient] for x_i in x])
+    def eval_grad(self, params: np.ndarray, x: np.ndarray=None, idx: int=0):
+        """
+        Evaluate gradient of u with respect to parameters
+        Args:
+            params (np.ndarray): parameter values
+            x (np.ndarray): x values
+            idx (int): setting index
+        Returns:
+            np.ndarray: gradient of u with respect to parameters
+        """
+        if x is None:
+            x = self.x_obs_  # Use observed x values if none provided
 
-    def eval_hessian(self, x, params, idx=0):
         if idx is None:
-            idx = 0
+            idx = 0  # Default index is 0
+        return np.array([[grad(x_i, self.F_vals[idx], *params) for grad in self.gradient] for x_i in x])  # Evaluate gradient
 
-        hessian = np.zeros((len(x), self.n_params, self.n_params))
+    def eval_hessian(self, params: np.ndarray, x: np.ndarray=None, idx: int=0) -> np.ndarray:
+        """
+        Evaluate Hessian of u with respect to parameters
+        Args:
+            params (np.ndarray): parameter values
+            x (np.ndarray): x values
+            idx (int): setting index
+        Returns:
+            np.ndarray: Hessian of u with respect to parameters
+        """
+        if x is None:
+            x = self.x_obs_  # Use observed x values if none provided
+
+        if idx is None:
+            idx = 0  # Default index is 0
+
+        hessian = np.zeros((len(x), self.n_params, self.n_params))  # Initialize Hessian array
         for i, x_i in enumerate(x):
             for j, hessian_row in enumerate(self.hessian):
                 for k, hess in enumerate(hessian_row):
-                    hessian[i, j, k] = hess(x_i, self.F_vals[idx], *params)
+                    hessian[i, j, k] = hess(x_i, self.F_vals[idx], *params)  # Evaluate Hessian
         return hessian
 
-    def get_dim(self):
-        return self.n_params
+    def get_dim(self) -> int:
+        """
+        Get dimension of the model
+        Returns:
+            int: dimension of the model
+        """
+        return self.n_params  # Return number of parameters
 
     def get_n_settings(self):
-        return self.n_settings_
+        """
+        Get number of settings
+        Returns:
+            int: number of settings
+        """
+        return self.n_settings_  # Return number of settings
 
 
 if __name__ == '__main__':
 
     # Example usage
-    F = [1., 2.]
+    F = np.array([1., 2.])
+    x_obs = np.array([0.1, 0.2])
     n_params = 2
-    model = ForwardModel(F, n_params)
-    print(model.eval_E(np.array([0.1, 0.2, 0.3, 0.6, 1.1]), np.array([0.1, 0.2])))
-
+    model = PiecewiseConstantModel(F, n_params, x_obs)
+    print(model.eval_E(np.array([0.1, 0.2]), np.array([0.1, 0.2, 0.3, 0.6, 1.1])))
 
     # x = np.linspace(0, 1, 100)
     x = np.array([0.1, 0.2, 0.3, 0.6, 1.0])
@@ -105,8 +234,8 @@ if __name__ == '__main__':
         grads = np.zeros((params_all.shape[0], len(x)))
         u = np.zeros((params_all.shape[0], len(x)))
         for i, params in enumerate(params_all):
-            grads[i] = model.eval_grad(x, params, j)[:,1]
-            u[i] = model.eval(x, params, j)
+            grads[i] = model.eval_grad(params, x, j)[:, 1]
+            u[i] = model.eval(params, x, j)
 
         fig, ax = plt.subplots(1, 1, figsize=(10, 5))
 
