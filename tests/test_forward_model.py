285c31fc2a681afa5ded70acd1c33ed13b863588
leon@workstation_home_new <l.f.riccius@tudelft.nl>
2024-12-11 19:11:06 +0100

    added LinearModel and unit testst for LinearModel and PiecewiseConstantModel

---
+++ tests/test_forward_model.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+from pdmp.forward_model import LinearModel, PiecewiseConstantModel
+from pdmp.utils import grad_fd, hessian_fd
+
+SMALL = -1e-6
+LARGE = 1e6
+
+
+def test_linear_model():
+
+    rng = np.random.default_rng(0)
+
+    n, m = rng.integers(2, 10, size=2)
+    # n, m = 3, 2
+    A = rng.random((n, m))
+    b = rng.random(n)
+    model = LinearModel(A, b)
+
+    # test gradient
+    x = rng.random(m)
+    grad = model.eval_grad(x)
+    grad_finite_diff = grad_fd(model.eval, x, n=n)
+    assert np.allclose(grad, grad_finite_diff, atol=1e-5)
+
+    # test hessian
+    hess = model.eval_hessian(x)
+    hess_finite_diff = hessian_fd(model.eval, x, n=n)
+    assert np.allclose(hess, hess_finite_diff, atol=1e-5)
+
+def test_piecewise_constant_model():
+
+    rng = np.random.default_rng(0)
+
+    n, n_obs, n_loc = 3, 2, 5
+    F = rng.random(n_obs)
+    x_obs = np.linspace(0, 1, n_loc + 1)[1:]
+    model = PiecewiseConstantModel(F, n, x_obs)
+
+    # test gradient
+    params = rng.random(n)
+    grad = model.eval_grad(params)
+    grad_finite_diff = grad_fd(model.eval, params, n=n_loc)
+    assert np.allclose(grad, grad_finite_diff, atol=1e-5)
+
+    # test hessian
+    hess = model.eval_hessian(params)
+    hess_finite_diff = hessian_fd(model.eval, params, n=n_loc, h=1e-5)
+    assert np.allclose(hess, hess_finite_diff, atol=1e-5)
+
+if __name__ == '__main__':
+    # test_linear_model()
+    test_piecewise_constant_model()
